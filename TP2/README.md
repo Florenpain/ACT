23d974bc14f89956bc1532a799b7fd7e34bad09c
Florentin <florentin.havart@kpf-si.com>
2022-09-29 17:48:43 +0200

    debut

---
+++ TP2/README.md
@@ -0,0 +1,9 @@
+# TP2
+
+## Question 1
+
+Formule mathématique:
+
+Si il y a une valeur négative on prend le max parmis les valeurs négatives, et elle égale à l'opposé du max plus 1
+Sinon on prend l'opposé du max -1
+
---
+++ TP2/main.java
@@ -0,0 +1,25 @@
+package TP2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+    
+    public static void main(String[] args) {
+        System.out.println("Question 2");
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("hauteur:");
+        String hauteur= sc.nextLine();
+        System.out.println("Largeur:");
+        String largeur= sc.nextLine();
+        
+        System.out.println("Les lignes de configuration");
+
+
+        for(int i=0; i<int(hauteur);i++){
+            String ligne= sc.nextLine();
+        }
+
+    }
+}
